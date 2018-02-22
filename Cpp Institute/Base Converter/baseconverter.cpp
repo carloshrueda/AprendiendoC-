b28c8f3a4a8d49d5d9d90e07364ddf355cd79ecd
carloshrueda <carloshrueda@gmail.com>
2018-02-22 10:20:13 -0500

    Base Converter
    - Este proyecto consiste en convertir un numero entero positovo a la 
    base que se pida.
    - Se agregó el proyecto.
    - Se construyo, probó y terminó.

---
+++ Cpp Institute/Base Converter/baseconverter.cpp
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   baseconverter.cpp
+ * Author: CRUEDA
+ *
+ * Created on 22 de febrero de 2018, 08:51 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char **argv) {
+    int n, b;
+
+    char num[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                  'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    cin >> n;
+    cin >> b;
+
+    if (n < b) {
+        cout << num[n] << endl;
+
+    } else {
+        int r, lenvr;
+        char vr[66];
+
+        lenvr = -1;
+        while (n >= b) {
+            r = n % b;
+            vr[++lenvr] = num[r];
+
+            n = n / b;
+        }
+        vr[++lenvr] = num[n];
+
+        for (int i = lenvr; i >= 0; i--) {
+            cout << vr[i];
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
