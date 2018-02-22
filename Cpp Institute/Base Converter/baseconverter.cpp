2ee8cf54d9f90b846937c9f2f9e84de74b2585af
carloshrueda <carloshrueda@gmail.com>
2018-02-22 10:49:41 -0500

    Base Converter
    - Formateo del código

--- Cpp Institute/Base Converter/baseconverter.cpp
+++ Cpp Institute/Base Converter/baseconverter.cpp
@@ -20,13 +20,13 @@ using namespace std;
  */
 int main(int argc, char **argv) {
     int n, b;
-    int lenvr= -1;
+    int lenvr = -1;
     char vr[66];
 
     char num[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-                  'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+        'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
     cin >> n;
     cin >> b;
